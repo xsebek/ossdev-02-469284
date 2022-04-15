f5635ac99955eb7acf5940658120a1808986318c
Ondrej Sebek <xsebek@fi.muni.cz>
2022-04-15 16:03:08 +0200

    Test against reference implementation

--- tests/unit.py
+++ tests/unit.py
@@ -2,6 +2,23 @@ import unittest
 from primezz import fact, is_prime, primes
 
 
+def reference_primes(x, y):
+    """Boring noob solution from:
+    https://www.geeksforgeeks.org/python-program-to-print-all-prime-numbers-in-an-interval
+    """
+    prime_list = []
+    for i in range(x, y):
+        if i == 0 or i == 1:
+            continue
+        else:
+            for j in range(2, int(i/2)+1):
+                if i % j == 0:
+                    break
+            else:
+                prime_list.append(i)
+    return prime_list
+
+
 class PrimeExamples(unittest.TestCase):
     def test_fact(self):
         self.assertEqual(fact(5), 5*4*3*2*1)
@@ -20,7 +37,13 @@ class PrimeExamples(unittest.TestCase):
         self.assertTrue(is_prime(-2))
 
     def test_primality_example(self):
+        """Test the example from documentation. """
         self.assertEqual(list(primes(11, 42)), [11, 13, 17, 19, 23, 29, 31, 37, 41])
+
+
+class ReferencePrime(unittest.TestCase):
+    def test_primes_500(self):
+        self.assertEqual(list(primes(0, 500)), reference_primes(0, 500))
 
 
 if __name__ == '__main__':
